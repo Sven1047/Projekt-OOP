aa3e6d7e44e3283e621e9cd78641877fb7ba39a8
Sven1047 <svengemperli@gmail.com>
2026-09-15 09:17:17 +0200

    --

---
+++ Ohne OOP/Game.java
@@ -0,0 +1,9 @@
+public class Game {
+    public Game() {
+        System.out.println("Game started...");
+    }
+    public void addPlayer(Player player) {
+    }
+    public void runGame() {
+    }
+}
---
+++ Ohne OOP/Player.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Player {
+    private String name;
+    private Scanner scanner;
+
+    public Player() {
+        this.scanner = new Scanner(System.in);
+    }
+}
